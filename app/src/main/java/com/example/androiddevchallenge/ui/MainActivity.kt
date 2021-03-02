3162bed9261b1d4d59d93ac9a8964ed935ef92f1
yangjinpeng009 <yangjinpeng009@ke.com>
2021-03-02 14:08:03 +0800

    fix lint

--- app/src/main/java/com/example/androiddevchallenge/ui/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/MainActivity.kt
@@ -1,15 +1,24 @@
-package com.example.androiddevchallenge.page
+package com.example.androiddevchallenge.ui
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Surface
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
